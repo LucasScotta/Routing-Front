78609efbdf9f4ad3ffe0c28236376a0ac0798ee6
LucasScotta <lucasmanuelscotta@gmail.com>
2022-09-11 05:06:17 -0300

    page Home added

--- src/Pages/Public/Home/Home.tsx
+++ src/Pages/Public/Home/Home.tsx
@@ -1,1 +1,24 @@
-export const Home = () => <>HOME</>
+import { useSelector } from 'react-redux'
+import { AppStore } from '../../../redux/store'
+import { PublicRoutes } from '../../../Routes'
+import './style/css.css'
+
+export const Home = () => {
+    const userState = useSelector((store: AppStore) => store.user)
+    const { name } = userState
+
+    return <section className="home">
+        <h1>Welcome{!!name && ' ' +name}!</h1>
+        <p>This page is hosted on netlify and Heroku, developed with React-Routing, Redux and TypeScript. I hope you enjoy it</p>
+        <p>Special thanks to <a href="https://aztro.sameerkumar.website/">Aztro</a> for its Horoscope's API</p>
+        <div className="home-actions">
+            <p>Sections</p>
+            <ul>
+                <li><a href={`/${PublicRoutes.LOGIN}/`}>Login</a></li>
+                <li><a href={`/${PublicRoutes.SIGNUP}/`}>Signin</a></li>
+                <li><a href={`/${PublicRoutes.SMF}`}>Semafore</a></li>
+                <li><a href={`/${PublicRoutes.HOROSCOPE}`}>Horoscope</a></li>
+            </ul>
+        </div>
+    </section>
+}
