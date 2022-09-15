17ae8fdc5423c32d99f338faa21152b12711f352
LucasScotta <lucasmanuelscotta@gmail.com>
2022-09-15 05:12:02 -0300

    navbar moved to Components

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import { PublicRoutes } from '../../Routes'
+import './style/css.css'
+
+export const Navbar = () => {
+    
+    return (
+        <header>
+            <nav className="navbar">
+                <ul className="sections">
+                    <li><a href={PublicRoutes.PUBLIC}>Home</a></li>
+                    <li><a href={PublicRoutes.PALETTE}>Palette</a></li>
+                    <li><a href={PublicRoutes.SMF}>Semaphore</a></li>
+                    <li><a href={PublicRoutes.HOROSCOPE}>Horoscope</a></li>
+                </ul>
+                <span></span>
+            </nav>
+        </header>
+    )
+}
